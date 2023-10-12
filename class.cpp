3839d093d2b585562748d02a5b166c52627e73cb
Meenal-oak <gpta.meenal1234@gmail.com>
2023-10-13 02:13:31 +0530

    Initial commit

---
+++ class.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class student{
+  public:
+  int age;
+  int roll_num;
+  string name;
+};
+int main(){
+
+  student s1;
+  s1.age = 19;
+  s1.name="meenal";
+  s1.roll_num = 21;
+  cout << s1.name<<" "<<s1.age <<" "<<s1.roll_num;
+
+  return 0;
+}
